73ff9ddfe180337dc1a26664df8d171d38fdec69
JeremSSJ <108894297+JeremSSJ@users.noreply.github.com>
2022-10-30 19:19:45 +0100

    window for the game

--- main.cpp
+++ main.cpp
@@ -39,86 +39,92 @@ int main()
         while (window.pollEvent(event))
         {
             if(event.type == Event::Closed) window.close() ;
-//            if(event.type == sf::Event::KeyReleased)
-//            {
-//                if(event.key.code == sf::Keyboard::Up)
-//                {
-//                    menu.MoveUp();
-//                }
-//                else if(event.key.code == sf::Keyboard::Down)
-//                {
-//                    menu.MoveDown();
-//                }
-//                else if(event.key.code == sf::Keyboard::Return)
-//                {
-//                    int pressedItem = menu.GetPressedItem();
-//
-//                    if(pressedItem == 0)
-//                    {
-//                        RenderWindow play(VideoMode(W, H), "Play Option");
-//                        window.close();
-//
-//                        while(play.isOpen())
-//                        {
-//                            play.clear();
-//                            play.display();
-//
-//
-//                            sf::Event event1;
-//                            while (play.pollEvent(event1))
-//                            {
-//
-//                                if(event1.type == sf::Event::Closed)
-//                                {
-//                                    play.close();
-//                                }
-//                            }
-//
-//                        }
-//                    }
-//                    else if(pressedItem == 1)
-//                    {
-//                        std::cout << "Option button has been pressed" << std::endl;
-//                    }
-//                    else
-//                    {
-//                        window.close();
-//                    }
-//                }
-//            }
-//            else if(event.type == sf::Event::Closed)
-//            {
-//                window.close();
-//            }
+            if(event.type == sf::Event::KeyReleased)
+            {
+                if(event.key.code == sf::Keyboard::Up)
+                {
+                    menu.MoveUp();
+                }
+                else if(event.key.code == sf::Keyboard::Down)
+                {
+                    menu.MoveDown();
+                }
+                else if(event.key.code == sf::Keyboard::Return)
+                {
+                    int pressedItem = menu.GetPressedItem();
+
+                    if(pressedItem == 0)
+                    {
+                        window.close();
+                        RenderWindow play(VideoMode(windowWidth, windowHeight), "Play Option");
+                        play.setFramerateLimit(9);
+
+                        while(play.isOpen())
+                        {
+                            sf::Event event1;
+                            while (play.pollEvent(event1))
+                            {
+
+                                if(event1.type == sf::Event::Closed)
+                                {
+                                    play.close();
+                                }
+                            }
+                            if (Keyboard::isKeyPressed(Keyboard::Up)) playground.changeDirectionPlayer1(0, -1);
+                            if (Keyboard::isKeyPressed(Keyboard::Down)) playground.changeDirectionPlayer1(0, 1);
+                            if (Keyboard::isKeyPressed(Keyboard::Left)) playground.changeDirectionPlayer1(-1, 0);
+                            if (Keyboard::isKeyPressed(Keyboard::Right)) playground.changeDirectionPlayer1(1, 0);
+
+                            if (Keyboard::isKeyPressed(Keyboard::Z)) playground.changeDirectionPlayer2(0, -1);
+                            if (Keyboard::isKeyPressed(Keyboard::S)) playground.changeDirectionPlayer2(0, 1);
+                            if (Keyboard::isKeyPressed(Keyboard::Q)) playground.changeDirectionPlayer2(-1, 0);
+                            if (Keyboard::isKeyPressed(Keyboard::D)) playground.changeDirectionPlayer2(1, 0);
+
+                            playground.movePlayers() ;
+
+
+                            player1Lost = playground.isPlayer1HasLost() ;
+                            player2Lost = playground.isPlayer2HasLost() ;
+
+                            if(player1Lost || player2Lost) play.close() ;
+
+                            playground.displayplayers() ;
+
+                            game.drawGame() ;
+
+                            play.clear();
+                            play.draw(game.getSprite());
+                            play.display();
+                        }
+
+                        if(player1Lost && player2Lost) std::cout << "Match draw" << std::endl ;
+                        else if(player2Lost) std::cout << "Player 1 won" << std::endl ;
+                        else if(player1Lost) std::cout << "Player 2 won" << std::endl ;
+                    }
+                    else if(pressedItem == 1)
+                    {
+                        std::cout << "Option button has been pressed" << std::endl;
+                    }
+                    else
+                    {
+                        window.close();
+                    }
+                }
+            }
+            else if(event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
         }
 
-        if (Keyboard::isKeyPressed(Keyboard::Up)) playground.changeDirectionPlayer1(0, -1);
-        if (Keyboard::isKeyPressed(Keyboard::Down)) playground.changeDirectionPlayer1(0, 1);
-        if (Keyboard::isKeyPressed(Keyboard::Left)) playground.changeDirectionPlayer1(-1, 0);
-        if (Keyboard::isKeyPressed(Keyboard::Right)) playground.changeDirectionPlayer1(1, 0);
-
-        if (Keyboard::isKeyPressed(Keyboard::Z)) playground.changeDirectionPlayer2(0, -1);
-        if (Keyboard::isKeyPressed(Keyboard::S)) playground.changeDirectionPlayer2(0, 1);
-        if (Keyboard::isKeyPressed(Keyboard::Q)) playground.changeDirectionPlayer2(-1, 0);
-        if (Keyboard::isKeyPressed(Keyboard::D)) playground.changeDirectionPlayer2(1, 0);
-
-        playground.movePlayers() ;
 
 
-        player1Lost = playground.isPlayer1HasLost() ;
-        player2Lost = playground.isPlayer2HasLost() ;
-
-        if(player1Lost || player2Lost) window.close() ;
-
-        playground.displayplayers() ;
-
-        game.drawGame() ;
+        //game.drawGame() ;
         window.clear();
-        window.draw(game.getSprite()); // affiche le nouveau sprite
+        menu.draw(window);
+        //window.draw(game.getSprite()); // affiche le nouveau sprite
         window.display();
     }
 
-    if(player1Lost && player2Lost) std::cout << "Match draw" << std::endl ;
-    else if(player2Lost) std::cout << "Player 1 won" << std::endl ;
-    else if(player1Lost) std::cout << "Player 2 won" << std::endl ;
+
 }
