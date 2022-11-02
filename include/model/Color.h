e92e5c4f1c5ec11004b278e6e9122cb789c01f2f
Leclercq David <93372601+XhaiiCo@users.noreply.github.com>
2022-11-02 10:12:50 +0100

    Add comments for color class

--- include/model/Color.h
+++ include/model/Color.h
@@ -1,10 +1,11 @@
 #ifndef COLOR_H
 #define COLOR_H
 
-
+//This class represents a color
 class Color
 {
     private:
+        //RGB value
         int red ;
         int green ;
         int blue ;
@@ -15,12 +16,15 @@ class Color
         Color(const Color& other);
         Color& operator=(const Color& other);
 
+        //This method valid that the color value is between 0 and 255
         bool validColor(int value) ;
 
+        //Setters
         bool setRed(int value) ;
         bool setGreen(int value) ;
         bool setBlue(int value) ;
 
+        //Getters
         int getRed(){ return this->red ;}
         int getGreen(){ return this->green ;}
         int getBlue(){ return this->blue ;}
